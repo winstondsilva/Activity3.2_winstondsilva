05d96467c2bb352e8925cd1f55a518c5c5b0c3b4
winstondsilva <winstondesilva0421@gmail.com>
2023-11-04 19:23:54 +0800

    feat/update-delete-inactive

--- hero.sql
+++ hero.sql
@@ -161,3 +161,15 @@ VALUES
 ALTER TABLE Item
 ADD item_price DECIMAL (10,2);
 
+--4	Create new branch named "feat/update-delete-inactive"
+
+-- Update the state of Hero 1 to inactive
+UPDATE Hero
+SET is_active = 'false'
+WHERE hero_id = 1;
+
+-- Deleting items associated with Hero 1
+DELETE FROM HeroItem 
+WHERE hero_id = 1;
+
+
